0a05070d5c01d28025906cff0962021c07d82a51
Sam <77967471+draumaz@users.noreply.github.com>
2022-01-25 09:36:46 -0800

    Add files via upload

---
+++ c/curses-reference.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <curses.h> // <3
+
+// -lncurses -lm (Linux/BSD: -ltinfo)
+
+int main() {
+        initscr(); // let's get this going
+        noecho(); // don't you fucking dare
+        raw(); // we like it like that
+        curs_set(0); // boolean boys
+        clear(); // a new screen just for us
+        move(0, 0); // (y,x) for some reason???
+        printw("\nPress a key.\n");
+        refresh(); // like blitting but for curses
+        keypad(stdscr, true); // make the arrows actually fucking work
+        int v; v = getch();
+        switch (v) {
+                case KEY_UP:
+                        printw("UP!");
+                        refresh();
+                        break;
+                case KEY_DOWN:
+                        printw("DOWN!");
+                        refresh();
+                        break;
+        } sleep(1);
+        endwin(); // fuck off
+        curs_set(1); // bring that back
+        system("stty sane"); // or 'pause' if you use windows
+        return 0;
+}
