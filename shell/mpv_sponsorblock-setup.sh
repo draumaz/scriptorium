19f67e7b3eb29d9e7201f2962ba13629dd76ad75
Sam <77967471+draumaz@users.noreply.github.com>
2022-02-10 15:51:05 -0800

    Update mpv_sponsorblock-setup.sh

--- shell/mpv_sponsorblock-setup.sh
+++ shell/mpv_sponsorblock-setup.sh
@@ -2,12 +2,13 @@ export lua_pf="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponso
 export py_pf="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponsorblock/sponsorblock_shared/sponsorblock.py.patch"
 export sb_url="https://github.com/po5/mpv_sponsorblock"
 export wget_args="--quiet --show-progress"
+export mpv_scripts_loc="$HOME/.config/mpv/scripts"
 
-mkdir -pv $HOME/.config/mpv/scripts
-echo "Moving into $HOME/.config/mpv/scripts" && cd $HOME/.config/mpv/scripts;
+mkdir -pv $mpv_scripts_loc
+echo "Moving into $mpv_scripts_loc" && cd $mpv_scripts_loc
 git clone $sb_url
-mv -v mpv_sponsorblock/sponsorblock.lua ./sponsorblock.lua | sed 's/renamed //g'
-mv -v mpv_sponsorblock/sponsorblock_shared . | sed 's/renamed //g'
+mv -v mpv_sponsorblock/sponsorblock.lua ./sponsorblock.lua
+mv -v mpv_sponsorblock/sponsorblock_shared ./sponsorblock_shared
 rm -rf mpv_sponsorblock
 wget $wget_args $lua_pf
 wget $wget_args $py_pf
