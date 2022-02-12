95b079238de24e758832507a8d0dd6e6071ade50
Sam <77967471+draumaz@users.noreply.github.com>
2022-02-12 14:11:10 -0800

    Reorganization

--- shell/mpv_sponsorblock-ricer.sh
+++ shell/mpv_sponsorblock-ricer.sh
@@ -1,13 +1,12 @@
-export lua_pf="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponsorblock/sponsorblock.lua.patch"
-export py_pf="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponsorblock/sponsorblock_shared/sponsorblock.py.patch"
-export sb_url="https://github.com/po5/mpv_sponsorblock"
+export PATCH_URL="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponsorblock"
+export PO5_SB_URL="https://github.com/po5/mpv_sponsorblock"
 
 mkdir -pv $HOME/.config/mpv/scripts
 echo "Moving into $HOME/.config/mpv/scripts" && cd $HOME/.config/mpv/scripts;
-git clone $sb_url
+git clone $PO5_SB_URL
 mv -v mpv_sponsorblock/sponsorblock.lua ./sponsorblock.lua
 mv -v mpv_sponsorblock/sponsorblock_shared .
 rm -rf mpv_sponsorblock
-patch sponsorblock.lua <(curl $lua_pf)
-patch sponsorblock_shared/sponsorblock.py <(curl $py_pf)
+patch sponsorblock.lua <(curl $PATCH_URL/sponsorblock.lua.patch)
+patch sponsorblock_shared/sponsorblock.py <(curl $PATCH_URL/sponsorblock_shared/sponsorblock.py.patch)
 rm -rfv sponsorblock.lua.patch sponsorblock.py.patch LICENSE README.md
