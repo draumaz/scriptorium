e55113068ffd669087881f44d5bff30e4b897ace
Sam <77967471+draumaz@users.noreply.github.com>
2022-02-12 14:03:14 -0800

    Remove wget dependency, patch pipes implemented

---
+++ shell/mpv_sponsorblock-ricer.sh
@@ -0,0 +1,13 @@
+export lua_pf="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponsorblock/sponsorblock.lua.patch"
+export py_pf="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponsorblock/sponsorblock_shared/sponsorblock.py.patch"
+export sb_url="https://github.com/po5/mpv_sponsorblock"
+
+mkdir -pv $HOME/.config/mpv/scripts
+echo "Moving into $HOME/.config/mpv/scripts" && cd $HOME/.config/mpv/scripts;
+git clone $sb_url
+mv -v mpv_sponsorblock/sponsorblock.lua ./sponsorblock.lua
+mv -v mpv_sponsorblock/sponsorblock_shared .
+rm -rf mpv_sponsorblock
+patch sponsorblock.lua <(curl -s $lua_pf)
+patch sponsorblock_shared/sponsorblock.py <(curl -s $py_pf)
+rm -rfv sponsorblock.lua.patch sponsorblock.py.patch LICENSE README.md
