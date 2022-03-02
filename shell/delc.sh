9cb91bfd5f3c5340cdb00a78f472ffc4b69cf30f
draumaz <77967471+draumaz@users.noreply.github.com>
2022-03-01 19:52:26 -0800

    improved flow

--- shell/delc.sh
+++ shell/delc.sh
@@ -1,11 +1,20 @@
 # Draumaz's Epic Layout Switcher
 # Switch between two keymaps on raw Xorg
 
-
 ## BEGIN CONFIG ##
+
 PRIMARY_KEYMAP="" # e.g. us
 SECONDARY_KEYMAP="" # e.g. ge
-## END CONFIG ##
+
+## END   CONFIG ##
+
+if [ "$PRIMARY_KEYMAP" == "" ]; then
+	echo "missing primary keymap config"
+	CFG_CHK=1
+fi; if [ "$SECONDARY_KEYMAP" == "" ]; then
+	echo "missing secondary keymap config"
+	CFG_CHK=1
+fi; if [ "$CFG_CHK" == "1" ]; then exit; fi
 
 FILE="/tmp/delc.conf" # generate file to monitor layout
 touch $FILE
@@ -19,12 +28,12 @@ fi
 
 VAR=$(cat $FILE)
 
-if [ "$VAR" == "1" ]; then # I imagine this could be pretty easily
-	setxkbmap $PRIMARY_KEYMAP # expanded into a carousel, but I only
-	echo 0 > $FILE			    # needed two keymaps.
-	echo "switched to primary keymap."
+if [ "$VAR" == "1" ]; then
+	setxkbmap $PRIMARY_KEYMAP
+	echo 0 > $FILE
+	echo "switched to primary keymap ($PRIMARY_KEYMAP)."
 else if [ "$VAR" == "0" ]; then
 	setxkbmap $SECONDARY_KEYMAP
 	echo 1 > $FILE
-	echo "switched to secondary keymap."
+	echo "switched to secondary keymap ($SECONDARY_KEYMAP)."
 fi fi
