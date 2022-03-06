dbb3602061f8393a38764dc015486d1ba44d3c78
draumaz <77967471+draumaz@users.noreply.github.com>
2022-03-06 11:53:36 -0800

    added homepage variable

--- shell/drbr.sh
+++ shell/drbr.sh
@@ -2,9 +2,10 @@
 # drbr, by draumaz (2022) | GPL v3
 # tmux-based twibright links wrapper for st
 
-## TODO: 
-## close tmux session when links is exited
+export HOMEPAGE=""
+
+## TODO: end tmux session when user exits links
 
 st -e bash -c 'tmux new-session -d -s drbr; 
-	tmux send-keys -t drbr "links duckduckgo.com" enter; 
+	tmux send-keys -t drbr "links '$HOMEPAGE'" enter; 
 	tmux attach -t drbr'
