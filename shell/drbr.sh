28a7edc7759f2de5294275e64c5023e894f6934f
draumaz <77967471+draumaz@users.noreply.github.com>
2022-03-09 08:19:25 -0800

    implemented HOMEPAGE env var, tmux bar hidden

--- shell/drbr.sh
+++ shell/drbr.sh
@@ -2,8 +2,13 @@
 # drbr, by draumaz (2022) | GPL v3
 # tmux-based twibright links wrapper for st
 
-export HOMEPAGE=""
+if [ "$HOMEPAGE" == "" ]; then
+	export HOMEPAGE="" # Set your homepage here.
+fi
 
-st -e bash -c 'tmux new-session -d; 
-	tmux send-keys "links '$HOMEPAGE'; tmux kill-session" enter; 
+st -e sh -c 'tmux new-session -d;
+	tmux set status off;
+	tmux send-keys \
+		"links '$HOMEPAGE'; tmux kill-session" \
+	enter;
 	tmux attach'
