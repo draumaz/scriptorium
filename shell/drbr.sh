134b8f02de5f6651250ee798452ceda9cae23d39
draumaz <77967471+draumaz@users.noreply.github.com>
2022-03-10 13:13:45 -0800

    drbr can be launched 2 ways now

--- shell/drbr.sh
+++ shell/drbr.sh
@@ -2,13 +2,12 @@
 # drbr, by draumaz (2022) | GPL v3
 # tmux-based twibright links wrapper for st
 
+new_win_st() {
+	st -e sh -c 'tmux new-session -d && tmux set status off && tmux send-keys "links '$HOMEPAGE' && tmux kill-session" enter && tmux attach'
+}
+
 if [ "$HOMEPAGE" == "" ]; then
 	export HOMEPAGE="" # Set your homepage here.
 fi
 
-st -e sh -c 'tmux new-session -d;
-	tmux set status off;
-	tmux send-keys \
-		"links '$HOMEPAGE'; tmux kill-session" \
-	enter;
-	tmux attach'
+if [ "$1" == "-w" ]; then new_win_st; else links $HOMEPAGE; fi
