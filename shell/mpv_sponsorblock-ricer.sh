41c1d84b8eb62b3069eb87f73e0f5c9f4c601bb7
Sam <77967471+draumaz@users.noreply.github.com>
2022-03-20 13:52:37 -0700

    Update mpv_sponsorblock-ricer.sh

--- shell/mpv_sponsorblock-ricer.sh
+++ shell/mpv_sponsorblock-ricer.sh
@@ -1,12 +1,14 @@
 export PATCH_URL="https://raw.githubusercontent.com/draumaz/patches/main/mpv_sponsorblock"
 export PO5_SB_URL="https://github.com/po5/mpv_sponsorblock"
 
-mkdir -pv $HOME/.config/mpv/scripts
-echo "Moving into $HOME/.config/mpv/scripts" && cd $HOME/.config/mpv/scripts;
+SCR_PATH="$HOME/.config/mpv/scripts"
+
+mkdir -pv $SCR_PATH
+echo "Moving into $SCR_PATH" && cd $SCR_PATH;
 git clone $PO5_SB_URL
 mv -v mpv_sponsorblock/sponsorblock.lua ./sponsorblock.lua
 mv -v mpv_sponsorblock/sponsorblock_shared .
 rm -rf mpv_sponsorblock
 patch sponsorblock.lua <(curl $PATCH_URL/sponsorblock.lua.patch)
 patch sponsorblock_shared/sponsorblock.py <(curl $PATCH_URL/sponsorblock_shared/sponsorblock.py.patch)
-rm -rfv sponsorblock.lua.patch sponsorblock.py.patch LICENSE README.md
+rm -rfv LICENSE README.md
