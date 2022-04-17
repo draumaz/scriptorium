c53e918d03f83b65451fe5a51ebd2e9c8a5152a9
Sam <77967471+draumaz@users.noreply.github.com>
2022-04-17 16:18:56 -0700

    Update pancurses.nix

--- nix/pancurses.nix
+++ nix/pancurses.nix
@@ -1,3 +1,8 @@
+# Enter a build environment that allows
+# for a successful pancurses Rust compile.
+# Requires cargo to have already been installed,
+# preferably locally through rustup.
+
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.stdenv.mkDerivation {
